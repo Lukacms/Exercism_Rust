78437dd2a402c3c2eec4c3d4c8175b360779885e
Luka Camus <chloe.camus@epitech.eu>
2022-05-07 21:07:24 +0200

    [ADD] exo04

--- ex03/src/lib.rs
+++ ex03/src/lib.rs
@@ -5,3 +5,27 @@
 // lib
 //
 
+fn production_by_hour(speed_range: u8) -> f64 {
+    let default_hour: f64 = 221.0;
+    let result;
+
+    if speed_range <= 4 {
+        result = speed_range as f64 * default_hour * 1.0;
+    } else if speed_range <= 8 {
+        result = speed_range as f64 * default_hour * 0.9;
+    } else {
+        result = speed_range as f64 * default_hour * 0.77;
+    }
+    return result;
+}
+
+fn working_items_per_minute(speed: u8) -> u32 {
+    let hour: f64 = production_by_hour(speed);
+    let result: u32 = hour as u32 / 60;
+
+    return result;
+}
+
+fn main() {
+    println!("{}", working_items_per_minute(6));
+}
---
+++ ex04/src/lib.rs
@@ -0,0 +1,54 @@
+//
+// EPITECH PROJECT, 2022
+// exercices_rust
+// File description:
+// lib
+//
+
+#[derive(Clone, PartialEq, Debug)]
+
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+fn log(level: LogLevel, message: &str) -> String {
+    if level == LogLevel::Info {
+        return info(message);
+    }
+    if level == LogLevel::Warning {
+        return warn(message);
+    }
+    if level == LogLevel::Error {
+        return error(message)
+    }
+    return message.to_string();
+}
+
+fn info(message: &str) -> String {
+    let info_str: String = "[INFO]:".to_string();
+    let result: String = format!("{} {}", info_str, message);
+
+    return result;
+}
+
+fn warn(message: &str) -> String {
+    let warn_str: String = "[WARNING]:".to_string();
+    let result: String = format!("{} {}", warn_str, message);
+
+    return result;
+}
+
+fn error(message: &str) -> String {
+    let error_str: String = "[ERROR]:".to_string();
+    let result: String = format!("{} {}", error_str, message);
+
+    return result;
+}
+
+fn main() {
+    let oui: String = log(LogLevel::Info, "Warn.");
+
+    println!("{oui}");
+}
